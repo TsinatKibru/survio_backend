4b78adc4d5335e871fbe4c6250bb133bbbf19cd1
tsinatkibru <tsinat000@gmail.com>
2026-04-01 10:44:27 +0300

    chore: increase JWT access and refresh token lifetimes

--- survio/settings.py
+++ survio/settings.py
@@ -125,8 +125,8 @@ REST_FRAMEWORK = {
 
 # JWT Settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,

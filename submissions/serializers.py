1ed8493a9cfe53a97bd42f970f750a43967928dd
tsinatkibru <tsinat000@gmail.com>
2026-04-07 23:39:15 +0300

    feat: add Base64ImageField to AnswerSerializer to support image uploads via base64 strings

--- submissions/serializers.py
+++ submissions/serializers.py
@@ -1,11 +1,24 @@
+import base64
+import uuid
+from django.core.files.base import ContentFile
 from rest_framework import serializers
 from django.utils import timezone
 from .models import Submission, Answer
 from forms_builder.serializers import FormListSerializer
 
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            # format: data:image/jpeg;base64,.....
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            id = uuid.uuid4()
+            data = ContentFile(base64.b64decode(imgstr), name=f"{id}.{ext}")
+        return super().to_internal_value(data)
 
 class AnswerSerializer(serializers.ModelSerializer):
     question_label = serializers.SerializerMethodField()
+    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         model = Answer
